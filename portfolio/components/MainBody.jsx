e20fd65c30f7d955bed683c2203128e7d7695eff
Avnish Kumar <69550090+theavnishkumar@users.noreply.github.com>
2023-10-21 11:43:59 +0530

    Added Header and Main Body

---
+++ portfolio/components/MainBody.jsx
@@ -0,0 +1,18 @@
+
+function MainBody() {
+  return (
+    <div className="h-screen w-[100%] flex">
+        <div className="bg-red-400 h-screen w-1/2 px-24 justify-center flex flex-col">
+            <p className="text-3xl font-semibold  font-serif">I&apos;m Jhon Doe </p>
+            <p className="text-xl font-semibold  font-serif">I&apos;m Web Developer Based on NY City </p>
+            <button className="text-base font-normal font-serif w-32 outline-none border-none rounded-xl py-2 my-2 bg-cyan-500">Portfolio</button>
+        </div>
+
+        <div className="bg-blue-400 h-screen w-1/2">
+
+        </div>
+    </div>
+  )
+}
+
+export default MainBody
