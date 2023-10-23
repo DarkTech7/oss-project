dd46c23998fde711c7420cd5465643c861294f61
Avnish Kumar <69550090+theavnishkumar@users.noreply.github.com>
2023-10-23 09:57:36 +0530

    Add files via upload

---
+++ Algos/Java/TypeCasting.java
@@ -0,0 +1,10 @@
+// import java.util.Scanner;
+
+public class TypeCasting{
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        float a =49.99f;
+        int b=(int)a;
+        System.out.println(b);
+    }
+}
